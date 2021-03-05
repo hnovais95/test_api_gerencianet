f97166881dd3bc6da9a9dd484a9c49598e5c7503
Heitor Novais <47889013+hnovais95@users.noreply.github.com>
2021-03-05 09:13:20 -0300

    Initial commit

---
+++ app/credentials.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+# Put your app credentials here
+CREDENTIALS = {
+    'client_id': 'Insira aqui o Client_ID',
+    'client_secret': 'Insira aqui o Client_Secret',
+    'sandbox': True  # True para desenvolvimento e False para produção
+}
